63f187400b68b5ebd88ddca9c6ba8d3ebaeaf516
IT24100566 <it24100566@my.sliit.lk>
2026-04-14 23:21:44 +0530

    Convert frontend to regular directory

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import CounterClass from './components/CounterClass';
+import CounterFunction from './components/CounterFunction';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header/> 
+      {/* <h1>Welcome to the React App!</h1>
+      <CounterClass/>
+      <CounterFunction/> */}
+    </div>
+  );
+}
+
+export default App;
